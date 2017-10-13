c1b35fc7a4be0d3aae334297e3f3e56cb1dffd98
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-10-13 01:27:49 +0000

    First tests preimplemented

--- Tests/TypeSwiftTests/TypeSwiftTests.swift
+++ Tests/TypeSwiftTests/TypeSwiftTests.swift
@@ -2,15 +2,57 @@ import XCTest
 @testable import TypeSwift
 
 class TypeSwiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(TypeSwift().text, "Hello, World!")
+    func testVariableType() {
+        var variableType: VariableType? = VariableType(rawValue: "let")
+        XCTAssert(variableType?.swiftValue == "var")
+
+        variableType = VariableType(rawValue: "const")
+        XCTAssert(variableType?.swiftValue == "let")
+
+        variableType = VariableType(rawValue: "Gibberish")
+        XCTAssertNil(variableType)
+    }
+    
+    func testSwiftNumber() {
+        var num = SwiftNumber(rawValue: "Float")
+        XCTAssertNotNil(num)
+        
+        num = SwiftNumber(rawValue: "Double")
+        XCTAssertNotNil(num)
+        
+        num = SwiftNumber(rawValue: "Int")
+        XCTAssertNotNil(num)
+        
+        num = SwiftNumber(rawValue: "Gibberish")
+        XCTAssertNil(num)
+    }
+    
+    func testType() {
+        var type: Type? = Type(rawValue: "boolean")
+        XCTAssert(type?.swiftValue == "Bool")
+
+        type = Type(rawValue: "number/*Float*/")
+        XCTAssert(type?.swiftValue == "Float")
+        
+        type = Type(rawValue: "[number/*Float*/, string]")
+        XCTAssert(type?.swiftValue == "(Float, String)")
+        
+        type = Type(rawValue: "[number/*Float*/, [string, [number/*Int*/, boolean]]]")
+        XCTAssert(type?.swiftValue == "(Float, (String, (Int, Bool)))")
+        
+        type = Type(rawValue: "[[string, [number/*Int*/, boolean]], number/*Float*/]")
+        XCTAssert(type?.swiftValue == "((String, (Int, Bool)), Float)")
+        
+        type = Type(rawValue: "CustomType")
+        XCTAssert(type?.swiftValue == "CustomType")
+        
+        type = Type(rawValue: "customType")
+        XCTAssertNil(type)
     }
 
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testVariableDeclaration", testVariableType),
+        ("testSwiftNumber", testSwiftNumber),
+        ("testType", testType)
     ]
 }
