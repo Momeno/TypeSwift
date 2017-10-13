3702ff27157b062829f246cea9ecb233551748c8
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-10-13 09:50:48 +0000

    [WIP] TODO: add tests for TypeScript and implement TypeScript.init(rawValue:)

--- Sources/TypeSwift/TypeScript.swift
+++ Sources/TypeSwift/TypeScript.swift
@@ -7,8 +7,26 @@
 
 import Foundation
 
-enum TypeScript {
+enum TypeScript: RawRepresentable, SwiftStringConvertible {
     //case model(ModelDeclaration, String, ModelBody)
     //case conformingModel(ModelDeclaration, String, String, ModelBody)
     case interface(InterfaceDeclaration, String, InterfaceBody)
+    
+    var rawValue: String {
+        switch self {
+        case .interface(let dec, let name, let body):
+            return "\(dec.rawValue) \(name) \(body.rawValue)"
+        }
+    }
+    
+    var swiftValue: String {
+        switch self {
+        case .interface(let dec, let name, let body):
+            return "\(dec.swiftValue) \(name) \(body.swiftValue)"
+        }
+    }
+    
+    init?(rawValue: String) {
+        return nil
+    }
 }
