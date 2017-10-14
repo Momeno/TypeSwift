55920cb0cfd72f44938cadc0892dbca31d24d6b5
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-10-14 22:15:05 +0000

    update readme + exported API

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 # TypeSwift
 
 A set of tools for parsing TypeScript models into Swift ones
+
+## Features
+
+- [X] Classes
+- [X] Interfaces
+- [ ] Enums
+- [ ] Index Signatures
+
+## How To Use
+
+### Installation
+
+```swift
+.package(url: "https://github.com/valdirunars/TypeSwift.git", from: "0.0.1")
+```
+
+### Usage
+```swift
+try! TypeSwift.sharedInstance.convert(file: fileURL,
+                                 to: .swift,
+                                 output: outURL)
+
+// or alternatively
+let string: String? = TypeSwift.sharedInstance.convertedString(from: typescript, to: .swift)
+``
--- Sources/TypeSwift/TypeSwift.swift
+++ Sources/TypeSwift/TypeSwift.swift
@@ -1,3 +1,36 @@
+import Foundation
+
+public enum Language  {
+    case swift
+}
+
 public struct TypeSwift {
-    var text = "Hello, World!"
+    
+    static let sharedInstance = TypeSwift()
+    
+    public func convert(file: URL, to language: Language, output url: URL) throws {
+        let typeScript = try loadTypeScript(from: file)
+        switch language {
+        case .swift:
+            try typeScript?.swiftValue
+                .write(to: url, atomically: true, encoding: .utf8)
+        }
+    }
+    
+    public func convertedString(from typescript: String, to language: Language) -> String? {
+        let typeScript = loadTypeScript(from: typescript)
+        switch language {
+        case .swift:
+            return typeScript?.swiftValue
+        }
+    }
+    
+    private func loadTypeScript(from localURL: URL) throws -> TypeScript? {
+        let string = try String(contentsOf: localURL)
+        return loadTypeScript(from: string)
+    }
+    
+    private func loadTypeScript(from string: String) -> TypeScript? {
+        return TypeScript(rawValue: string)
+    }
 }
