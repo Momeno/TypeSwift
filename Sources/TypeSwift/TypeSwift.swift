68cd1ce1b4f648e34ae6861903c22ec80acc1d94
valdirunars <valdirunars@gmail.com>
2017-10-17 14:49:21 +0000

    minor

--- Sources/TypeSwift/TypeSwift.swift
+++ Sources/TypeSwift/TypeSwift.swift
@@ -6,7 +6,7 @@ public enum Language  {
 
 public struct TypeSwift {
     
-    public static let sharedInstance = TypeSwift()
+    public static let shared = TypeSwift()
     
     public func convert(file: URL, to language: Language, output url: URL) throws {
         let typeScript = try loadTypeScript(from: file)
